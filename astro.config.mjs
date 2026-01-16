2abcc6ac75475b1bc2efaec3bdacf3b8ffb382e2
Jin Yen Liang <itisalongway574@gmail.com>
2026-01-16 23:49:50 +0800

    chore: add astro-remove-comments integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import removeComments from 'astro-remove-comments';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jinyenliang.work',
+  integrations: [removeComments()],
   vite: {
     plugins: [tailwindcss()]
   }
